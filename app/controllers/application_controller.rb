8f45e7a54b9f42dee78b98c20bd2ee0ff6764846
wynterwolf <shinyguns@gmail.com>
2020-08-24 19:53:28 -0500

    two down

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,61 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+configure do
+  set :public_folder, 'public'
+  set :views, 'app/views'
+end
 
-  get '/' do
-  end
+# get '/' do
+# end
+
+get '/articles' do
+  @articles = Article.all
+  erb :index.html.erb
+end
+
+#create of CRUD
+get '/articles/new' do
+  erb :'new.html'
+end
+
+#create of CRUD
+post '/articles' do
+  article = Article.create(params)
+  # erb :new
+  redirect "/articles/#{article.id}" 
+end
+
+# Read of CRUD
+get '/articles' do
+  @articles = Article.all
+  erb :'index.html'
+end
+
+# READ of CRUD
+get '/articles/:id' do
+  @article = Article.find(params[:id])
+  erb :'show.html'
+end
+
+#UPDATE of crud
+get '/articles/:id/edit' do
+  @article = Article.find(params[:id])
+  erb :'edit.html'
+end
+
+#Update of CRUD
+patch '/articles/:id/edit' do
+  @article = Article.find(params[:id])
+  @article.update(params)
+  redirect "/meals/#{@meal.id}"
+end
+
+#DELETE of CRUD
+delete '/articles/:id' do
+  @article = Article.find(params[:id])
+  @article.destroy
+  redirect :articles
+end
 end
