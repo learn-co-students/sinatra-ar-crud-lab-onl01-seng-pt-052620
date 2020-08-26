630d48d1bb4b17b563c65264a34603df478b396d
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-08-26 14:45:58 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,22 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get "/articles" do 
+    @articles = Article.all
+    erb :index  
+  end 
+
+  get "/articles/:id" do 
+   @article = params[:id]
+   erb :show 
+  end  
+
+  get "/models/:id/edit" do 
+    erb :edit 
+  end 
+
+  delete "/articles/:id" do 
+    erb :show 
+  end 
 end
