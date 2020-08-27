b8aedadc856c6d959ca4f30debf84415cb22d12a
bebofofum <bri.boehm@gmail.com>
2020-08-27 19:18:22 -0400

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,19 +2,19 @@
 
 class Article < ActiveRecord::Base
 
-  attr_reader :title, :content
+  # attr_reader :title, :content
 
-  @@all = []
+  # @@all = []
 
-  def initialize(params)
-    @title = params[:title]
-    @content = params[:content]
-    @@all << self
-  end
+  # def initialize(params)
+  #   @title = params[:title]
+  #   @content = params[:content]
+  #   @@all << self
+  # end
 
-  def self.all
-    @@all
-  end
+  # def self.all
+  #   @@all
+  # end
 
 
 end
